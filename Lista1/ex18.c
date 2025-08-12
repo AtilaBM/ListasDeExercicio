fb6fd15cc59f00c91bd97531e3cab220a500f294
Atila <a7ilabm@gmail.com>
2025-08-12 20:12:47 -0300

    style change

--- Lista1/ex18.c
+++ Lista1/ex18.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-int main(){
-    float petFood,amountFood;
+int main()
+{
+    float petFood, amountFood;
     int days = 5;
 
-     printf("Insert the values for pet food and amount of food\n");
+    printf("Insert the values for pet food and amount of food\n");
     printf("-------------------------------\n");
 
     printf("Insert the pet food weight(kg): ");
-    scanf("%f",&petFood);
+    scanf("%f", &petFood);
     printf("Insert the amount of food for each cat(grams): ");
-    scanf("%f",&amountFood);
+    scanf("%f", &amountFood);
 
-    float amountFood5days = ((amountFood * 2) * 5)/1000;
-    //float kgToGram = petFood/1000;
+    float amountFood5days = ((amountFood * 2) * 5) / 1000;
+    // float kgToGram = petFood/1000;
     float remainFood = petFood - amountFood5days;
     printf("-------------------------------\n");
-    printf("After 5 days remain %.2f(kg) of pet food\n",remainFood);
+    printf("After 5 days remain %.2f(kg) of pet food\n", remainFood);
     printf("-------------------------------\n");
-
+    return 0;
 }
