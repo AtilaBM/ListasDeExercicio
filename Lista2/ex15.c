cdb0dcf21bbada01c843473f2344c5c4b6c9378b
Atila <a7ilabm@gmail.com>
2025-08-24 21:18:39 -0300

    Just started exercice 15

---
+++ Lista2/ex15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int hoursWorked, overtime, depedents;
+    float salary;
+
+    printf("=================Insert the values for Salary, Hours Worked, Overtime and depedents.\n");
+    printf("Salary: ");
+    scanf("%f", &salary);
+    printf("Hours Worked: ");
+    scanf("%f", &hoursWorked);
+    printf("Overtime: ");
+    scanf("%f", &overtime);
+    printf("Depedents: ");
+    scanf("%f", &depedents);
+
+    return 0;
+}
